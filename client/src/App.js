c54298a16adad6d70ebc9210b80223c397c1a25c
vicentefb <chenteferrara@gmail.com>
2021-12-04 21:46:16 -0600

    front end first configuration

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Typography, AppBar } from "@material-ui/core";
+
+const App = () => {
+  return (
+    <div>
+      <AppBar position="static" color="inherit">
+        <Typography variant="h2" align="center">
+          Video Chat
+        </Typography>
+      </AppBar>
+      {/*VideoPlayer*/}
+      {/*Options Notifications*/}
+    </div>
+  );
+};
+
+export default App;
